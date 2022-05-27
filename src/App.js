8732acdce4d423f32fa0e34b96b029923ab34f7b
Ankit Jha <jhaankit3117@gmail.com>
2022-05-27 23:26:57 +0530

    this is the quote generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+const App = () => {
+  const [newQuote,setNewQuote] = useState();
+  const [quoteAuthor,setQuoteAuthor] = useState();
 
-function App() {
+  const getNewQuote = async () => {
+     try{
+       let res = await fetch('https://api.quotable.io/random');
+       let data = await res.json();
+       console.log(data);
+       const author = data.author;
+       const content = data.content;
+      setNewQuote(content)
+      setQuoteAuthor(author)
+     }catch (error){
+       console.log(error);
+     }
+  }
+
+  useEffect( () => {
+    const fetchData = async () => {
+      await getNewQuote()
+    }
+    fetchData()
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='main-container'>
+      <div className='quote-main'>
+            <h1 className='quote'>"{newQuote}"</h1>
+            </div>
+            <span className='author'>
+            <h3><strong>-- {quoteAuthor}</strong></h3>
+            </span>
+        <div className='btn-container'>
+        <button className='btn' onClick={getNewQuote}>next</button>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
+
